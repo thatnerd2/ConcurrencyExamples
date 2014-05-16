bd443c728e573ce64a7847062f77a1d7ae7165bb
thatnerd2 <thatnerd2@gmail.com>
2014-05-16 14:12:43 -0400

    Working on SwingGUIMulti

--- ConcurrencyProject/src/standalone/SwingGuiMulti.java
+++ ConcurrencyProject/src/standalone/SwingGuiMulti.java
@@ -1,8 +1,38 @@
 package standalone;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 
 public class SwingGuiMulti {
+	private static SwingGuiMulti factory = new SwingGuiMulti();
+	private static JFrame frame;
 	
+	public static void main (String[] args) {
+		setupFrame();
+		setupButtons();
+		renderButtons();
+	}
+	
+	private static void setupFrame () {
+		frame = new JFrame("Swing GUI Example");
+		frame.setSize(400, 400);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+	
+	public static void setupButtons () {
+		JButton actionButton = new JButton ("Click to count to 5000");
+		JButton responseButton = new JButton ("Click to get an alert");
+		JButton[] buttonGroup = {actionButton, responseButton};
+		for (int i = 0; i < buttonGroup.length; i++) {
+			buttonGroup[i].setSize(200, 100);
+			buttonGroup[i].setLocation(100 * i*50, 200);
+			frame.add(buttonGroup[i]);
+		}
+		
+		
+	}
 	
 	
 }
